d2e5b0dbe4405ba1ccf20332b7ccaec5e8c1eec0
Rasmus Bækgaard <rbk@trifork.com>
2025-01-07 08:39:27 +0100

    Remove existing files from FUT, and update main files to be used for KIP

--- fhir/sitesearch.js
+++
@@ -1,5 +0,0 @@
-var domainroot = window.location.href.substring(0, window.location.href.lastIndexOf("/")) + "/";
-
-function Gsitesearch(curobj) {
-  curobj.q.value = "site:" + domainroot + " " + curobj.qfront.value;
-}
