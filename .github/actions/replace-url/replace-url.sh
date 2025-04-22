2e0afeeec454147c0ccffb15a679194a77a0e5ba
Mads Swensson <mgks@trifork.com>
2025-04-22 13:33:59 +0200

    update pipeline

--- .github/actions/replace-url/replace-url.sh
+++ .github/actions/replace-url/replace-url.sh
@@ -6,22 +6,20 @@ OLD_TAG=${OLD_TAG}
 NEW_TAG=${NEW_TAG}
 FILE_GLOB='**/*.html'
 
+#OLD_TAG='<a no-external="true" href="https://kip.rkkp.dk/fhir/history.html">Directory of published versions</a>'
+#NEW_TAG='<a no-external="true" href="https://kip-infrastructure.github.io/KIP-ig-website/fhir/history.html">Directory of published versions</a>'
+
+cd "$(git rev-parse --show-toplevel)"
+
 echo "Finding files matching glob: $FILE_GLOB"
-FILES=$(find . -type f -name "$FILE_GLOB")
+FILES=$(find . -type f -wholename "$FILE_GLOB")
 
 # Count how many files were found
 FILE_COUNT=$(echo "$FILES" | wc -l)
 
 echo "Found $FILE_COUNT HTML files matching pattern."
+echo -e "Replacing tag $OLD_TAG \n with \n $NEW_TAG"
 
-echo  "Replacing tag $OLD_TAG with $NEW_TAG"
-
-#for FILE in $FILES; do
-#  if grep -qF "$OLD_TAG" "$FILE"; then
-#    echo "Replacing tag in $FILE"
-#    sed -i.bak "s|$OLD_TAG|$NEW_TAG|g" "$FILE"
-#    rm "${FILE}.bak"
-#  fi
-#done
+echo "$FILES" | xargs -n 100 sed -i.bak "s|$OLD_TAG|$NEW_TAG|g"
 
 echo "Tag replacement complete."
