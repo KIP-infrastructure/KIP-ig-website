507be2ec006a3666b5e24a00926a803ded9a045b
Mads Swensson <mgks@trifork.com>
2025-04-22 14:13:12 +0200

    added generic funtion for searching throught the html files and replacing values

--- .github/actions/replace-url/replace-url.sh
+++ .github/actions/replace-url/replace-url.sh
@@ -2,22 +2,26 @@
 
 set -e
 
-OLD_TAG=${OLD_TAG}
-NEW_TAG=${NEW_TAG}
-FILE_GLOB='**/*.html'
+find_and_replace() {
+    OLD_TAG=$1
+    NEW_TAG=$2
+    FILE_GLOB='**/*.html'
 
-#OLD_TAG='<a no-external="true" href="https://kip.rkkp.dk/fhir/history.html">Directory of published versions</a>'
-#NEW_TAG='<a no-external="true" href="https://kip-infrastructure.github.io/KIP-ig-website/fhir/history.html">Directory of published versions</a>'
+    echo "Finding files matching glob: $FILE_GLOB and tag: $OLD_TAG"
+    FILES=$(rg --files-with-matches --glob "$FILE_GLOB" "$OLD_TAG")
+    
+    FILE_COUNT=$(echo "$FILES" | wc -l)
+    echo "Found $FILE_COUNT HTML files matching pattern."
+    echo -e "\nReplacing tag \n$OLD_TAG \nwith \n$NEW_TAG"
+    
+    echo "$FILES" | xargs -n 100 sed -i "s|$OLD_TAG|$NEW_TAG|g"
+    
+    echo -e "done\n"
+}
 
 cd "$(git rev-parse --show-toplevel)"
 
-echo "Finding files matching glob: $FILE_GLOB"
-FILES=$(find . -type f -wholename "$FILE_GLOB")
+find_and_replace '<a href="https://kip.rkkp.dk/fhir">' '<a href="https://kip-infrastructure.github.io/KIP-ig-website/fhir">' 
+find_and_replace '<a no-external="true" href="https://kip.rkkp.dk/fhir/history.html">Directory of published versions</a>' '<a no-external="true" href="https://kip-infrastructure.github.io/KIP-ig-website/fhir/history.html">Directory of published versions</a>'
 
-FILE_COUNT=$(echo "$FILES" | wc -l)
-echo "Found $FILE_COUNT HTML files matching pattern."
-echo -e "\nReplacing tag \n$OLD_TAG \nwith \n$NEW_TAG"
-
-echo "$FILES" | xargs -n 100 sed -i "s|$OLD_TAG|$NEW_TAG|g"
-
-echo -e "\nTag replacement complete."
+echo "Tag replacement complete."
